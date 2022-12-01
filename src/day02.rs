aa77e2710cb8fdd0c5930ac52e9e4d8f3a9e91ae
Peter Hart <peter.hart@supplydynamics.com>
2022-12-01 12:33:04 -0500

    day 02 setup

---
+++ src/day02.rs
@@ -0,0 +1,51 @@
+use std::fs;
+use core::str::Split;
+use itertools::Itertools;
+use itertools::sorted;
+use itertools::rev;
+
+fn sum_group(iter : &mut Split<&str>) -> Option<i32> {
+    match iter.next() {
+        None => None,
+        Some("") => Some(0),
+        Some(s) => Some(s.parse::<i32>().unwrap() + match sum_group(iter) {
+            None => 0,
+            Some(x) => x
+        })
+    }
+}
+
+fn part1(name: &str) -> i32 {
+    return fs::read_to_string(name)
+        .expect("Couldn't open file")
+        .split("\r\n")
+        .batching(sum_group)
+        .max().unwrap_or(0);
+}
+
+fn part2(name: &str) -> i32 {
+    return rev(sorted(fs::read_to_string(name)
+        .expect("Couldn't open file")
+        .split("\r\n")
+        .batching(sum_group))).take(3).sum();
+}
+
+fn main() {
+    println!("Part 1: {}", part1("data/Day02.txt"));
+    println!("Part 2: {}", part2("data/Day02.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(part1("data/Day02_test.txt"), 24000);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2("data/Day02_test.txt"), 45000);
+    }
+}
