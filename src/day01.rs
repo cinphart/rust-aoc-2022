091ff5d6425a252cd854cd9d200d514325505f6b
Peter Hart <peter.hart@supplydynamics.com>
2022-12-01 11:48:25 -0500

    day 01 solutions

--- src/day01.rs
+++ src/day01.rs
@@ -1,17 +1,33 @@
 use std::fs;
+use core::str::Split;
+use itertools::Itertools;
+use itertools::sorted;
+use itertools::rev;
 
-fn part1(name: &str) -> usize {
-    return fs::read_to_string(name)
-    .expect("Couldn't open file")
-    .split("\r\n")
-    .count();
+fn sum_group(iter : &mut Split<&str>) -> Option<i32> {
+    match iter.next() {
+        None => None,
+        Some("") => Some(0),
+        Some(s) => Some(s.parse::<i32>().unwrap() + match sum_group(iter) {
+            None => 0,
+            Some(x) => x
+        })
+    }
 }
 
-fn part2(name: &str) -> usize {
+fn part1(name: &str) -> i32 {
     return fs::read_to_string(name)
-    .expect("Couldn't open file")
-    .split("\r\n")
-    .count();
+        .expect("Couldn't open file")
+        .split("\r\n")
+        .batching(sum_group)
+        .max().unwrap_or(0);
+}
+
+fn part2(name: &str) -> i32 {
+    return rev(sorted(fs::read_to_string(name)
+        .expect("Couldn't open file")
+        .split("\r\n")
+        .batching(sum_group))).take(3).sum();
 }
 
 fn main() {
@@ -25,11 +41,11 @@ mod tests {
 
     #[test]
     fn part1_works() {
-        assert_eq!(part1("data/Day01_test.txt"), 0);
+        assert_eq!(part1("data/Day01_test.txt"), 24000);
     }
 
     #[test]
     fn part2_works() {
-        assert_eq!(part2("data/Day01_test.txt"), 0);
+        assert_eq!(part2("data/Day01_test.txt"), 45000);
     }
 }
