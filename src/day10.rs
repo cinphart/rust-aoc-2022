a96175807cd53fe21573bdc3a69f2a32da1f7174
Peter Hart <peter.hart@supplydynamics.com>
2022-12-12 09:06:32 -0500

    setup for day10-12

---
+++ src/day10.rs
@@ -0,0 +1,31 @@
+const INPUT: &str = include_str!("../data/Day10.txt");
+
+fn part1(input: &str) -> usize {
+    input.len()
+}
+
+fn part2(input: &str) -> usize {
+    input.len()
+}
+
+fn main() {
+    println!("Part 1: {}", part1(INPUT));
+    println!("Part 2: {}", part2(INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../data/Day10_test.txt");
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(part1(TEST_INPUT), 13140);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(TEST_INPUT), 8);
+    }
+}
